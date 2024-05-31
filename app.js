2a1836e31504603b286e790f5228826c38a33594
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-30 19:35:58 -0500

    added event listener to button

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const queryTerm = form.elements.query.value;
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${queryTerm}`);
+    console.log(res.data);
+})
